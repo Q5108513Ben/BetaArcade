a59c885039053446a2b485efd1c41dd07e27fa83
Ben <q5108513@live.tees.ac.uk>
2018-10-11 21:58:55 +0100

    [WIP] Camera Fade In / Out
    Working on getting the camera to fade in and out when transitioning
    between rooms to make the transition feel smoother. Currently using
    coroutines although the first call seems to have a delay, need to find a
    fix for this or use an alternative method that doesn't use coroutines.

--- BetaArcade/Assets/Development Folder/Holdsworth, Ben/CameraController.cs
+++ BetaArcade/Assets/Development Folder/Holdsworth, Ben/CameraController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
@@ -27,6 +29,17 @@ public class CameraController : MonoBehaviour {
     // If the camera's dimensions reach the max / min value we need to prevent it from
     // moving any further in that direction. The max / min values will be retrieved 
     // from the current room the player is in, for now they are hardcoded for testing.
+
+    [Tooltip("The image we are fading in and out to. Recommended to just keep this as a black image.")]
+    public Image fade_image;
+    [Tooltip("The speed it takes for the image to change from 0 to 1 opactity. (0 being transparent)")]
+    public float fade_in_time = 0;
+    [Tooltip("The speed it takes for the image to change from 1 to 0 opactity. (0 being transparent)")]
+    public float fade_out_time = 0;
+    [Tooltip("This value determines how long the image will linger on 1 opacity.")]
+    public float fade_blackout_time = 0;
+
+    private bool isBlack = true;
 
     void Update() {
 
@@ -78,6 +91,8 @@ public class CameraController : MonoBehaviour {
 
     public void UpdateRoomBoundry(RoomBoundary new_boundary) {
 
+        StartCoroutine(Fade());
+
         Camera camera = GetComponent<Camera>();
 
         float camera_height = camera.orthographicSize * 2.0f;
@@ -87,6 +102,50 @@ public class CameraController : MonoBehaviour {
         xMinValue = new_boundary.boundary_left + (camera_width / 2);
         yMaxValue = new_boundary.boundary_top - (camera_height / 2);
         yMinValue = new_boundary.boundary_bottom + (camera_height / 2);
+        
+    }
+
+    IEnumerator Fade() {
+
+        while (!isBlack) {
+
+            var temp_colour = fade_image.color;
+            temp_colour.a += fade_in_time * Time.deltaTime;
+            fade_image.color = temp_colour;
+
+            if (temp_colour.a >= 1) {
+
+                isBlack = true;
+
+                for (int i = 0; i < fade_blackout_time; i++)
+                {
+                    yield return null;
+                }
+
+            }
+
+            yield return null;
+
+        }
+
+        while (isBlack) {
+
+            var temp_colour = fade_image.color;
+            temp_colour.a -= fade_out_time * Time.deltaTime;
+            fade_image.color = temp_colour;
+
+            if (temp_colour.a <= 0) {
+
+                isBlack = false;
+
+            }
+
+            yield return null;
+
+        }
+
+        //yield return null;
 
     }
+
 }
