e3110985c7157145bd5bf140b752a24eb1f1162a
Q5108513Ben <q5108513@live.tees.ac.uk>
2018-12-12 15:47:04 +0000

    New Floor Textures.
    Added a bunch of new textures to the floor prefabs. Also began working
    on the bot counter feature, waiting on Farlan to create an image for it
    before fully implementing it.

---
+++ BetaArcade/Assets/Development Folder/Holdsworth, Ben/BotCounterWidget.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class BotCounterWidget : MonoBehaviour {
+
+    Image image;
+
+	void Start () {
+
+        image = GetComponent<Image>();
+
+        image.type = Image.Type.Filled;
+        image.fillMethod = Image.FillMethod.Radial360;
+        image.fillClockwise = true;
+        
+        image.fillAmount = 0;
+
+	}
+
+	void Update () {
+
+        image.fillAmount += 0.1f * Time.deltaTime;
+
+	}
+}
